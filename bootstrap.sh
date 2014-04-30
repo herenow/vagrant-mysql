b48ac04f77aa74fe9846205f6dea08c02a2ca3a4
herenow <leonardoshiro@gmail.com>
2014-04-30 02:41:34 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,9 @@ apt-get update
 apt-get install -y curl openjdk-7-jdk
 
 # Now install crate.io
-bash -c "$(curl -L try.crate.io)"
+curl -L try.crate.io > ./try.crate.io
+chmod 755 ./try.crate.io
+bash ./try.crate.io
 
+# Add it to boot
+ln -s ./trye.crate.io /etc/init.d/crate
