86c57163dbf69cac014d4c67823ae37a99a5010c
herenow <leonardoshiro@gmail.com>
2014-05-07 13:17:44 -0300

    vagrant doesnt run reload everytime
    vagrant doesnt run reload everytime, this script works better

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,11 @@
 apt-get update
 apt-get install -y curl openjdk-7-jdk
 
-# Download the install script
-curl -L try.crate.io > ./try.crate.io
-chmod 755 ./try.crate.io
+# Download and install crate
+bash -c $(curl -L try.crate.io)
 
-# Add it to boot
-ln -s /home/vagrant/try.crate.io /etc/init.d/crate
-
-# Now install crate.io
-bash ./try.crate.io
+# Boot Script
+sudo echo '#!/bin/sh' >> /etc/init.d/crate
+sudo echo '/home/vagrant/crate*/bin/crate' >> /etc/init.d/crate
+sudo chmod 755 /etc/init.d/crate
+sudo update-rc.d crate defaults
