164812e31a856b6553a46e7123b7274bdc3ee08c
herenow <leonardoshiro@gmail.com>
2014-05-07 15:34:19 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,11 @@
 apt-get update
 apt-get install -y curl openjdk-7-jdk
 
-# Download and install crate
-bash -c "$(curl -L try.crate.io)"
-
-# Boot Script
+# Boot script
 sudo echo '#!/bin/sh' >> /etc/init.d/crate
 sudo echo '/home/vagrant/crate*/bin/crate' >> /etc/init.d/crate
 sudo chmod 755 /etc/init.d/crate
 sudo update-rc.d crate defaults
+
+# Download and install crate
+bash -c "$(curl -L try.crate.io)"
